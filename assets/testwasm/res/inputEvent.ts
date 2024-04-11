0a3b623522f5600f5dbbbf91e3c2ef0e1ef95a31
linxi1990 <651761058@qq.com>
2024-04-11 08:41:38 +0800

    12121

--- assets/testwasm/res/inputEvent.ts
+++ assets/testwasm/res/inputEvent.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, EventTouch, Input, input, Camera, geometry, PhysicsSystem, PhysicsRayResult, Mat4, view, Vec3, Vec4, Texture2D, ImageAsset, BufferAsset, RenderTexture, SpriteFrame, Sprite } from 'cc';
+import { _decorator, Component, Node, EventTouch, Input, input, Camera, geometry, PhysicsSystem, PhysicsRayResult, Mat4, view, Vec3, Vec4, Texture2D, ImageAsset, BufferAsset, RenderTexture, SpriteFrame, Sprite, director, MeshRenderer, UITransform } from 'cc';
 import { graphics } from './graphics';
 const { ccclass, property } = _decorator;
 
@@ -23,8 +23,11 @@ export class inputEvent extends Component {
     @property(Sprite)
     sp:Sprite = null;
 
+    @property(MeshRenderer)
+    mesh:MeshRenderer = null;
+
     private tex = new Texture2D();
-    private r = 3;
+    private r = 10;
 
     private _ray: geometry.Ray = new geometry.Ray();
     private _mask: number = 0xffffffff;
@@ -38,6 +41,15 @@ export class inputEvent extends Component {
         let pixels = this.rt.readPixels(0, 0, width, height);
 
         this.createTexture();
+
+        input.on(Input.EventType.TOUCH_MOVE, (event:EventTouch) => {
+            let detal = event.touch.getDelta().x;
+            this.mesh.node.getComponent(UITransform);
+            let ang:Vec3 = new Vec3();
+            ang = this.mesh.node.eulerAngles.add3f(0, detal, 0);
+            this.mesh.node.eulerAngles = ang;
+            
+        })
 
         input.on(Input.EventType.TOUCH_START, (event: EventTouch) => {
             console.log("Touch Start");
@@ -68,12 +80,13 @@ export class inputEvent extends Component {
                         let fovy = Math.PI / 4;
                         let aspect = view.getViewportRect().width / view.getViewportRect().height
                         Mat4.perspective(pMat, fovy, aspect, 1, 1000)
-                        //pv矩阵
-                        let pvMat = new Mat4();
-                        Mat4.multiply(pvMat, pMat, viewMat);
+                        //mvp矩阵
+                        let mvpMat = new Mat4();
+                        // Mat4.multiply(mvpMat, viewMat, mode);
+                        Mat4.multiply(mvpMat, pMat, viewMat);
 
                         let hipPointTemp = new Vec4(r.hitPoint.x, r.hitPoint.y, r.hitPoint.z, 1);
-                        Vec4.transformMat4(ppos, hipPointTemp, pvMat);
+                        Vec4.transformMat4(ppos, hipPointTemp, mvpMat);
                         ppos.divide4f(ppos.w, ppos.w, ppos.w, ppos.w);
 
                         ppos.multiplyScalar(0.5).add4f(0.5, 0.5, 0.5, 0.5);
@@ -126,19 +139,27 @@ export class inputEvent extends Component {
     }
 
     drawAt(x:number, y:number){
-        let pixelBuf:ArrayBuffer = (this.tex.image.data as ArrayBufferView).buffer;
-        let dataView = new DataView(pixelBuf);
-        for(let i = 1; i <= this.r; i ++){
-            for(let j = 1; j <= this.r; j ++){
+        // let pixelBuf:ArrayBuffer = (this.tex.image.data as ArrayBufferView).buffer;
+
+        let pixelBuf:Uint8Array = this.readPixels()
+        let dataView = new DataView(pixelBuf.buffer);
+        for(let i = 0; i <= this.r; i ++){
+            for(let j = 0; j <= this.r; j ++){
                 if(x - i >=0 && y - j >= 0){
-                    dataView.setUint32((x-i)*1024 * 4 + (y-j)*4, 0xff0000ff, true);
+                    dataView.setUint32((x-i) * 4 + (y-j)*1024*4, 0xff000000, true);
                 }
 
                 if(x - i >= 0 && y + j < 1024){
-                    dataView.setUint32((x-i)*1024 * 4 + (y+j)*4, 0xff0000ff, true);
+                    dataView.setUint32((x-i) * 4 + (y+j)*1024*4, 0xff000000, true);
                 }
 
-                
+                if(x + 1 >= 0 && y - j >= 0){
+                    dataView.setUint32((x+i) * 4 + (y-j)*1024*4, 0xff0000ff, true);
+                } 
+
+                if(x + 1 >= 0 && y + j >= 0){
+                    dataView.setUint32((x+i) * 4 + (y+j)*1024*4, 0xff0000ff, true);
+                } 
             }
             // if(x - i >= 0 && y - i >= 0){
             //     dataView.setUint32((x-i)*1024 * 4 + (y-i)*4, 0xff0000ff, true);
@@ -152,7 +173,60 @@ export class inputEvent extends Component {
             //     dataView.setUint32((x+i)*1024 * 4 + (y-i)*4, 0xff0000ff, true);
             // }
         }
-        dataView.setUint32(x*1024 * 4 + y*4, 0xff0000ff, true);
+
+        // dataView.setUint32(x*1024 * 4 + y*4, 0xff0000ff, true);
+
+        // director.root.device.copyTextureToBuffers()
+
+        // director.root.device.copyBuffersToTexture()
+        this.tex.uploadData(pixelBuf);
+
+        this.mesh.materials[0].setProperty("mixTexture", this.tex);
+        // let img:ImageAsset = new ImageAsset()
+        // img.reset({
+        //     _data:new Uint8Array(pixelBuf),
+        //     _compressed:false,
+        //     width:1024,
+        //     height:1024,
+        //     format:Texture2D.PixelFormat.RGBA8888
+        // })
+        // this.tex.image = img;
+
+        // this.sp.spriteFrame.texture = this.tex;
+    }
+
+    public readPixels (x = 0, y = 0, width?: number, height?: number, buffer?: Uint8Array): Uint8Array | null {
+        width = width || 1024;
+        height = height || 1024;
+        const gfxTexture = this.tex.getGFXTexture();
+        if (!gfxTexture) {
+            // errorID(7606);
+            return null;
+        }
+        const needSize = 4 * width * height;
+        if (buffer === undefined) {
+            buffer = new Uint8Array(needSize);
+        } else if (buffer.length < needSize) {
+            // errorID(7607, needSize);
+            return null;
+        }
+
+        // const gfxDevice = this._getGFXDevice();
+        const gfxDevice = director.root.device;
+
+        const bufferViews: ArrayBufferView[] = [];
+        const regions: BufferTextureCopy[] = [];
+
+        const region0 = new BufferTextureCopy();
+        region0.texOffset.x = x;
+        region0.texOffset.y = y;
+        region0.texExtent.width = width;
+        region0.texExtent.height = height;
+        regions.push(region0);
+
+        bufferViews.push(buffer);
+        gfxDevice?.copyTextureToBuffers(gfxTexture, bufferViews, regions);
+        return buffer;
     }
 
     update(deltaTime: number) {
@@ -160,4 +234,76 @@ export class inputEvent extends Component {
     }
 }
 
+export class BufferTextureCopy {
+    declare private _token: never; // to make sure all usages must be an instance of this exact class, not assembled from plain object
 
+    constructor (
+        public buffOffset: number = 0,
+        public buffStride: number = 0,
+        public buffTexHeight: number = 0,
+        public texOffset: Offset = new Offset(),
+        public texExtent: Extent = new Extent(),
+        public texSubres: TextureSubresLayers = new TextureSubresLayers(),
+    ) {}
+
+    public copy (info: Readonly<BufferTextureCopy>) {
+        this.buffOffset = info.buffOffset;
+        this.buffStride = info.buffStride;
+        this.buffTexHeight = info.buffTexHeight;
+        this.texOffset.copy(info.texOffset);
+        this.texExtent.copy(info.texExtent);
+        this.texSubres.copy(info.texSubres);
+        return this;
+    }
+}
+
+export class TextureSubresLayers {
+    declare private _token: never; // to make sure all usages must be an instance of this exact class, not assembled from plain object
+
+    constructor (
+        public mipLevel: number = 0,
+        public baseArrayLayer: number = 0,
+        public layerCount: number = 1,
+    ) {}
+
+    public copy (info: Readonly<TextureSubresLayers>) {
+        this.mipLevel = info.mipLevel;
+        this.baseArrayLayer = info.baseArrayLayer;
+        this.layerCount = info.layerCount;
+        return this;
+    }
+}
+
+export class Offset {
+    declare private _token: never; // to make sure all usages must be an instance of this exact class, not assembled from plain object
+
+    constructor (
+        public x: number = 0,
+        public y: number = 0,
+        public z: number = 0,
+    ) {}
+
+    public copy (info: Readonly<Offset>) {
+        this.x = info.x;
+        this.y = info.y;
+        this.z = info.z;
+        return this;
+    }
+}
+
+export class Extent {
+    declare private _token: never; // to make sure all usages must be an instance of this exact class, not assembled from plain object
+
+    constructor (
+        public width: number = 0,
+        public height: number = 0,
+        public depth: number = 1,
+    ) {}
+
+    public copy (info: Readonly<Extent>) {
+        this.width = info.width;
+        this.height = info.height;
+        this.depth = info.depth;
+        return this;
+    }
+}
